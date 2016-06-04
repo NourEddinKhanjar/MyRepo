4768e43c6b17a00b0836b0fa482a3b9f9a45cc20
Nour Eddin Khanjar <nour.kh.9345@gmail.com>
2016-06-04 15:16:41 +0300

    first push

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({   
+    selector: 'toh-app',
+    templateUrl: 'app.component.html'
+})
+
+
+export class AppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
